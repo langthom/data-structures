71f23ad6da42af467af5bf89ca7513b3261d9e29
langthom <Langi1@gmx.at>
2014-07-02 21:29:17 +0200

    added bubble sort algorithm in Java

---
+++ sortingAlgorithms/BubbleSort/Java/BubbleSort.java
@@ -0,0 +1,31 @@
+/**
+ * Implementation of the Bubble sort algorithm.
+ *
+ * @author Thomas Lang
+ * @version 0.1
+ */
+public class BubbleSort<T extends Comparable<T>>{
+
+    /**
+     * Empty constructor.
+     */
+    public BubbleSort(){}
+
+    /**
+     * Implementation of the Bubble sort algorithm.
+     *
+     * @param  T[] array
+     *                   The array to sort
+     */
+    public static <T extends Comparable<T>> void bubbleSort( T[] array ){
+        for( int i = array.length; i > 0; i-- ){
+            for( int j = 0; j < i - 1; j++ ){
+                if( array[j].compareTo( array[j+1] ) > 0 ){
+                    T tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
+            }
+        }
+    }
+}

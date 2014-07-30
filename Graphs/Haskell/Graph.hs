eafe35bc03f8ed9c37b821ee9282c1129291e67e
langthom <Langi1@gmx.at>
2014-07-30 13:37:30 +0200

    added first part of graph in Haskell

---
+++ Graphs/Haskell/Graph.hs
@@ -0,0 +1,70 @@
+{- |
+Module      :   Graph.hs
+Description :   Implementation of a simple Graph.
+Copyright   :   (c) Thomas Lang, 2014
+License     :   BSD 3
+
+Stability   :   stable
+Portability :   portable
+
+This module implements a typical DIRECTED graph.
+Such a graph is simple a list of edges, while an 
+edge is a tuple from the first Node to the second
+one. Furthermore it implements a few functions
+and algorithms that are typical for graphs, like
+the testing if two nodes are adjacent, or the
+depth-first search.
+ -}
+module Graph(
+              Graph,        -- ^ data type
+              adj,          -- ^ are two nodes adjacent?
+              adjList,      -- ^ adjList of a Node
+              dfs           -- ^ depth-first-search
+            )where
+
+
+-- --------------------------------------------------------------
+-- type synonyms representing a directed graph
+-- --------------------------------------------------------------
+type Node a = a
+
+type Edge a = (Node a,Node a)
+
+type Graph a = [Edge a]
+
+
+-- sample graph (for testing purposes only)
+test :: Graph Integer
+test = [(1,2),(2,3),(2,2),(1,4),(4,4)]
+
+
+
+-- --------------------------------------------------------------
+-- checks if two passed nodes are adjacent
+-- --------------------------------------------------------------
+adj :: Eq a => Node a -> Node a -> Graph a -> Bool
+adj v w g = (v,w) `elem` g
+
+
+
+-- --------------------------------------------------------------
+-- returns a list of edges that are adjacent to the passed node
+-- --------------------------------------------------------------
+adjList :: Eq a => Node a -> Graph a -> Graph a
+adjList v g = filter (\(x,y) -> x==v) g
+
+
+
+-- --------------------------------------------------------------
+-- performs a depth-first search on the passed graph
+-- Note that the starting Node ist the first one 
+-- appearing in the Graph.
+--
+-- Note: not working yet
+-- --------------------------------------------------------------
+dfs :: Eq a => Graph a -> [Node a]
+dfs [] = []
+dfs g  = let top = fst (head g)
+         in top : dfs (adjList top (tail g))
+
+

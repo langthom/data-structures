df971d3b645f8fd0dbb340703b2d5d5077950834
langthom <Langi1@gmx.at>
2014-08-16 21:08:39 +0200

    added topological sorting and test if graph is azyclical

--- Graphs/Haskell/Graph.hs
+++ Graphs/Haskell/Graph.hs
@@ -7,13 +7,14 @@ License     :   BSD 3
 Stability   :   stable
 Portability :   portable
 
-This module implements a typical DIRECTED graph.
+This module implements a typical DIRECTED AZYCLICAL 
+graph.
 Such a graph is simple a list of edges, while an
 edge is a tuple from the first Node to the second
 one. Furthermore it implements a few functions
 and algorithms that are typical for graphs, like
 the testing if two nodes are adjacent, or the
-depth-first search.
+topologic sorting algorithm.
  -}
 module Graph(
               Graph,        -- ^ data type
@@ -21,7 +22,9 @@ module Graph(
               adjList,      -- ^ adjList of a Node
               contains,     -- ^ is a node in the graph?
               add,          -- ^ adds a new edge to graph
-              remove        -- ^ removes a node from the graph
+              remove,       -- ^ removes a node from the graph
+              topSort,      -- ^ topological sorting
+              isAzyclical   -- ^ is graph azyclical?
             )where
 
 import Data.List
@@ -39,7 +42,7 @@ type Graph a = [Edge a]
 
 -- sample graph (for testing purposes only)
 test :: Graph Integer
-test = [(1,2),(2,3),(2,2),(1,4),(4,4)]
+test = [(1,2),(2,3),(1,4),(2,4)]
 
 
 
@@ -89,3 +92,43 @@ remove :: Eq a => Node a -> Graph a -> Maybe (Graph a)
 remove n [] = Nothing
 remove n g | g `contains` n = Just $ g \\ filter (\(u,_) -> u == n) g
            | otherwise      = Nothing
+
+
+
+
+-- ---------------------------------------------------------------
+-- performs the topological sorting algorithm
+-- ---------------------------------------------------------------
+topSort :: Eq a => Graph a -> [Node a]
+topSort g = let x = ts g
+                y = map fst x ++ map snd x
+            in nub y
+    where ts :: Eq a => Graph a -> Graph a 
+          ts [] = []
+          ts g  = let f = sources g 
+                  in f ++ ts (g \\ f)
+
+
+
+-- ---------------------------------------------------------------
+-- gets all edges which first nodes are
+-- sources (which has no ingoing edges)
+-- ---------------------------------------------------------------
+sources :: Eq a => Graph a -> Graph a
+sources g = filter (\e -> not (fst e `elem` f)) g
+    where f = map snd g
+
+
+
+-- ---------------------------------------------------------------
+-- checks if the passed graph is azyclical.
+-- a graph is azyclical, if the graph reduced
+-- by all edges given through topSort is 
+-- the empty list.
+-- ---------------------------------------------------------------
+isAzyclical :: Eq a => Graph a -> Bool
+isAzyclical g = g \\ ts == []
+    where ts = [(u,v)|u <- topSort g, v <- s]
+          s  = [x | let a = map snd g, x <- a]
+
+

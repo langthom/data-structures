32ec84728d519f5c528e9c27cb87b41be9d92080
Thomas Lang <langthom@fim.uni-passau.de>
2014-08-27 11:01:29 +0200

    second try to fix, still not working

--- sortingAlgorithms/BubbleSort/MIPS/bubble_sort.asm
+++ sortingAlgorithms/BubbleSort/MIPS/bubble_sort.asm
@@ -48,17 +48,15 @@ array:	.word	0:10			# declaring array of 10 integers
 	j 	ALL_DONE
 
 
-SORT:	addi	$sp,	$sp,	-12	# prepare stack
-	sw	$ra,	8($sp)		# save return address
-	sw	$a0,	4($sp)		# save argument: array base-address
-	sw	$a1,	0($sp)		# save argument: size
+SORT:	addi	$sp,	$sp,	-4	# prepare stack
+	sw	$ra,	0($sp)		# save return address
 	
 	li	$s0,	0		# int ready = 0;
 	add	$s1,	$a1,	$zero	# int i = size;
 	li	$s2,	0		# int j = 0;
 
 L1:	beq	$s0,	1,	DONE	# first loop condition
-	sgt	$t0,	$s1,	0	# second loop condition
+	sgt	$t0,	$s1,	$zero	# second loop condition
 	beq	$t0,	$zero,	DONE	
 	li	$s0,	1		# set ready to 1
 	
@@ -74,11 +72,11 @@ L2:	addi	$t0,	$s1,	-1	# $t0 = i - 1;
 	lw	$t2,	($s2)		# write A[j] to $t2
 	lw	$t3,	($s3)		# write A[j+1] to $t3
 	sgt	$t4,	$t2,	$t3	# $t4 = (A[j] > A[j+1]) ? 1 : 0
-	beq	$t4,	0,	NOPE	
+	beq	$t4,	$zero,	NOPE	
 	
 	add	$t5,	$t2,	$zero	# int tmp = A[j];
-	sw	$t2,	($s3)		# A[j] = A[j+1]
-	sw	$t3,	($s2)		# A[j +1] = tmp
+	sw	$t3,	($s2)		# A[j] = A[j+1]
+	sw	$t5,	($s3)		# A[j+1] = tmp
 	
 	li	$s0,	0		# set ready to 0
 	
@@ -88,11 +86,9 @@ NOPE:	addi	$s2,	$s2,	1	# j++
 DONE2:	addi	$s1,	$s1,	-1	# i--
 	j	L1			# jump for looping in L1
 
-DONE:					# all done, return to caller:
-	lw	$a1,	0($sp)		# restore all arguments and 
-	lw	$a0,	4($sp)		# 	return adress
-	lw	$ra,	8($sp)
-	addi	$sp,	$sp,	8	# restoring stack
+DONE:					
+	lw	$ra,	0($sp)		# return adress
+	addi	$sp,	$sp,	4	# restoring stack
 	
 	jr	$ra			# final jump to caller
 	
